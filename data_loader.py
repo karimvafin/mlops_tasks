2d1719e258b5dcbba932160500c0b20840e0c084
karimvafin <karim.vafin@mail.ru>
2025-08-29 21:33:46 +0300

    Update files

--- data_loader.py
+++ data_loader.py
@@ -2,7 +2,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder
 
-data = pd.read_csv("heart.csv")
+data = pd.read_csv("data/heart.csv")
 
 feature_cols = ['age', 'sex', 'cp', 'trestbps', 'chol', 'fbs',
                 'restecg', 'thalach', 'exang', 'oldpeak', 'slope', 'ca', 'thal']
